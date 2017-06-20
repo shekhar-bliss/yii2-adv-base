4228252cf2c61f483cff3fd1c2fc5ccf81c34291
Shekhar <chandrashekhar.thalkar@infinitylabs.in>
2017-06-20 12:36:35 +0530

    Config backend as per yii2-advanced-one-domain-config

--- backend/config/main.php
+++ backend/config/main.php
@@ -10,10 +10,12 @@ return [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
+    'homeUrl' => '/admin',
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
         'request' => [
+            'baseUrl' => '/admin',
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
