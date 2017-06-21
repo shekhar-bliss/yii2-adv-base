ee459cddb211b939abbcbb7f294c6d346ff3e101
Shekhar <chandrashekhar.thalkar@infinitylabs.in>
2017-06-22 01:29:00 +0530

    Theme login page as per required design

--- frontend/themes/adminltecustom/site/login.php
+++ frontend/themes/adminltecustom/site/login.php
@@ -19,52 +19,58 @@ $fieldOptions2 = [
 ];
 ?>
 
-<div class="login-box">
-    <div class="login-logo">
-        <a href="#"><b>Admin</b>LTE</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
 
-        <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
+  <body class="login-bg">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="login-box">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="model-intro">
+                                    <h3>service model</h3>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card card-container">
+                                    <img id="profile-img" class="profile-img-card" src="<?= Yii::$app->request->baseUrl.'/images/user-icon.png' ?>">
+                                    <p id="profile-name" class="profile-name-card"><?= Html::encode($this->title) ?></p>
 
-        <?= $form
-            ->field($model, 'username', $fieldOptions1)
-            ->label(false)
-            ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+                                    <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
 
-        <?= $form
-            ->field($model, 'password', $fieldOptions2)
-            ->label(false)
-            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+                                    <?= $form
+                                        ->field($model, 'username', $fieldOptions1)
+                                        ->label(false)
+                                        ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
 
-        <div class="row">
-            <div class="col-xs-8">
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                                    <?= $form
+                                        ->field($model, 'password', $fieldOptions2)
+                                        ->label(false)
+                                        ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+
+                                    <div class="row">
+                                        <div class="col-xs-8">
+                                            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                                        </div>
+                                        <!-- /.col -->
+                                        <div class="col-xs-4">
+                                            <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
+                                        </div>
+                                        <!-- /.col -->
+                                    </div>
+
+
+                                    <?php ActiveForm::end(); ?>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- /.col -->
-            <div class="col-xs-4">
-                <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
-            </div>
-            <!-- /.col -->
         </div>
 
-
-        <?php ActiveForm::end(); ?>
-
-        <div class="social-auth-links text-center">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in
-                using Facebook</a>
-            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign
-                in using Google+</a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
-
     </div>
-    <!-- /.login-box-body -->
-</div><!-- /.login-box -->
