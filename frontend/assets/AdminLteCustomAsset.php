f3798aafad5c11a49b665b77ac5527f0cba7989f
Shekhar <chandrashekhar.thalkar@infinitylabs.in>
2017-06-22 11:53:39 +0530

    Define Admin LTE Custom theme's asset bundle for Admin LTE Custom theme

---
+++ frontend/assets/AdminLteCustomAsset.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * AdminLTE Custom frontend application asset bundle.
+ */
+class AdminLteCustomAsset extends AssetBundle
+{
+	public $sourcePath = '@app/themes/adminltecustom';
+    public $css = [
+        'css/custom-style.css',
+    ];
+}
